c7b6787ec37e2b4d40c46f5250f5560adc796cad
Carlos35654E <arriolacarlos64@gmail.com>
2025-09-11 22:01:25 -0600

    remake
    cambié todo para poner la página del proyecto final de módulo 1.5

--- js/app.js
+++ js/app.js
@@ -1,33 +1,51 @@
-function borrar() {
-    let name = document.getElementById('name');
-    let mail = document.getElementById('mail');
-    let messag = document.getElementById('message');
+// Variables para controlar el carrusel
+const pista = document.querySelector('.carrusel-pista');
+const diapositivas = Array.from(pista.children);
+const siguienteBtn = document.querySelector('.next-btn');
+const anteriorBtn = document.querySelector('.prev-btn');
+const carruselContainer = document.querySelector('.carrusel-imagen-principal');
 
-    console.log('Nombre: ', name.value);
-    console.log('Correo: ', mail.value);
-    console.log('Mensaje: ', message.value);
+const tamañoDiapositiva = diapositivas[0].getBoundingClientRect().width;
+let indiceDiapositiva = 0;
 
+// Variables para el movimiento automático
+let autoplayInterval;
+const autoplayDelay = 3000;
 
-    setTimeout(() => {
-        name.value = '';
-        mail.value = '';
-        message.value = '';
-    }, 1000);
+siguienteBtn.addEventListener('click', () => {
+    indiceDiapositiva++;
 
-}
-
-function cambiarColor() {
-
-    const rutaActual = window.location.pathname;
-
-    const enlaces = document.querySelectorAll('nav a');
-
-    enlaces.forEach(enlace => {
-
-    if (enlace.href.includes(rutaActual)) {
-        enlace.classList.add('active');
+    if (indiceDiapositiva >= diapositivas.length) {
+        indiceDiapositiva = 0;
     }
-    });
+
+    const nuevaPosicion = -tamañoDiapositiva * indiceDiapositiva;
+    pista.style.transform = `translateX(${nuevaPosicion}px)`;
+});
+
+anteriorBtn.addEventListener('click', () => {
+    indiceDiapositiva--;
+
+    if (indiceDiapositiva < 0) {
+        indiceDiapositiva = diapositivas.length - 1;
+    }
+
+    const nuevaPosicion = -tamañoDiapositiva * indiceDiapositiva;
+    pista.style.transform = `translateX(${nuevaPosicion}px)`;
+});
+
+function startAutoplay() {
+    autoplayInterval = setInterval(() => {
+        siguienteBtn.click();
+    }, autoplayDelay);
 }
 
-cambiarColor()
+function stopAutoplay() {
+    clearInterval(autoplayInterval);
+}
+
+carruselContainer.addEventListener('mouseenter', stopAutoplay);
+
+carruselContainer.addEventListener('mouseleave', startAutoplay);
+
+startAutoplay();
